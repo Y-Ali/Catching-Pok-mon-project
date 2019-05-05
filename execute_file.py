ad1a667dfa19cdf876877f340582e7b6e33d6e0a
Yousuf Ali <yousuf-123@hotmail.co.uk>
2019-05-05 17:59:47 +0100

    created two more methods and updated previous methods

--- execute_file.py
+++ execute_file.py
@@ -1,30 +1,24 @@
 from player_class import Player
 from pokemon_class import Pokemon
 
-# hi = Player()
-# hi.search_for_pokemon()
-class ExecutionFile(Player):
+# def ask_for_name(self):
+#     asked = False
+#     while not asked:
+#         user_name = input("Hi!, What's your name? > ")
+#         self.name = user_name
+#         asked = True
+#
+# def ask_to_capture_pokemon(self):
+#     asked = False
+#     while not asked:
+#
+#         capture_pokemon = input("Do you want to capture a Pokemon!? > ")
+#         if capture_pokemon == "y":
+#             print("We've found a", )
 
-    def __init__(self):
-        super().__init__()
-
-
-    # def ask_for_name(self):
-    #     asked = False
-    #     while not asked:
-    #         user_name = input("Hi!, What's your name? > ")
-    #         self.name = user_name
-    #         asked = True
-    #
-    # def ask_to_capture_pokemon(self):
-    #     asked = False
-    #     while not asked:
-    #
-    #         capture_pokemon = input("Do you want to capture a Pokemon!? > ")
-    #         if capture_pokemon == "y":
-    #             print("We've found a", )
 b = Player()
-b.search_for_pokemon()
+b.ask_for_name()
+#b.search_for_pokemon()
 
 
 
--- player_class.py
+++ player_class.py
@@ -1,3 +1,4 @@
+import random
 from pokemonNames.pokemonNames import PokemonNames
 
 class Player:
@@ -7,28 +8,59 @@ class Player:
         self.city = ''
         self.caught_pokemon = []
 
+    def ask_for_name(self):
+
+        user_name = input("Hi!, What's your name? > ")
+        self.name = user_name
+
+        self.search_for_pokemon()
+
     def search_for_pokemon(self):
         call_pokemon = PokemonNames()
         get_random_pokemon_name = call_pokemon.get_random_name()
 
-        # print(get_random_pokemon_name)
-        # print(self.name)
-
-        asked = False
-        while not asked:
-            user_name = input("Hi!, What's your name? > ")
-            self.name = user_name
-            asked = True
-            print("generated Pokémon  ----->", get_random_pokemon_name)
-
-            self.__try_to_catch_pokemon(get_random_pokemon_name)
+        print("generated Pokémon  ----->", get_random_pokemon_name)
+        self.__try_to_catch_pokemon(get_random_pokemon_name)
 
     def __try_to_catch_pokemon(self, get_random_pokemon_name):
         asked = False
+        print("We've found a", get_random_pokemon_name, "!")
+
         while not asked:
-            capture_pokemon = input("Do you want to try and capture a Pokémon!? y/n > ")
+            capture_pokemon = input(f"Do you want to try and capture a {get_random_pokemon_name}!? y/n > ")
             if capture_pokemon == "y":
-                print("We've found a", get_random_pokemon_name, "!")
+                try_can_catch = random.randint(1,10)
+
+                if try_can_catch % 2 == 0:
+                    self.caught_pokemon.append(get_random_pokemon_name)
+                    print(f"We caught a{get_random_pokemon_name}!")
+
+                else:
+                    print("failed to catch")
+            asked = True
+        print("This is the list", self.caught_pokemon)
+
+        try_again = input("Would you like to search for another Pokémon?")
+        if try_again == "y":
+            self.search_for_pokemon()
+
+    def save_player_and_pokemon(self):
+        pass
+
+    def load_previous_player(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
 
     def save_player_and_pokemon(self):
         pass

9339a235148495a9df2832e364aa6ade4eaa8f66
Yousuf Ali <yousuf-123@hotmail.co.uk>
2019-05-06 16:19:33 +0100

    refactured code. edited pokemon class

--- player_class.py
+++ player_class.py
@@ -21,12 +21,12 @@ class Player:
         call_pokemon = PokemonNames()
         get_random_pokemon_name = call_pokemon.get_random_name()
 
-        print("generated Pokémon  ----->", get_random_pokemon_name)
+        #print("generated Pokémon  ----->", get_random_pokemon_name)
         self.__try_to_catch_pokemon(get_random_pokemon_name)
 
     def __try_to_catch_pokemon(self, get_random_pokemon_name):
         asked = False
-        print("We've found a", get_random_pokemon_name, "!")
+        print("We've found a ", get_random_pokemon_name , "!")
 
         while not asked:
             capture_pokemon = input(f"Do you want to try and capture a {get_random_pokemon_name}!? y/n > ")
@@ -35,41 +35,33 @@ class Player:
 
                 if try_can_catch % 2 == 0:
                     self.caught_pokemon.append(get_random_pokemon_name)
-                    print(f"We caught a{get_random_pokemon_name}!")
-                    print("---------------------->",self.caught_pokemon)
+                    print(f"We caught a {get_random_pokemon_name}!")
                 else:
                     print("failed to catch")
-                    print("---------------------->", self.caught_pokemon)
+                    #print("---------------------->", self.caught_pokemon)
             asked = True
-            print("---------------------->", self.caught_pokemon)
+            #print("---------------------->", self.caught_pokemon)
             try_again = input("Would you like to search for another Pokémon?")
             if try_again == "y":
-                print("---------------------->", self.caught_pokemon)
                 self.search_for_pokemon()
-            elif try_again == "n":
+
+            else:
                 print("---------------------->", self.caught_pokemon)
 
                 self.save_player_and_pokemon(get_random_pokemon_name)
 
-
     def save_player_and_pokemon(self, get_random_pokemon_name):
         try:
             if get_random_pokemon_name in self.caught_pokemon:
-                print("---------------------->", self.caught_pokemon)
 
-                str1 = ','.join(self.caught_pokemon)
-
-                #print(type(str1))
-                #print("entered the if statement.....")
+                str1 = ','.join(self.caught_pokemon)    # cast each item in list to a string.
 
                 query = (f"INSERT INTO Player (player_name, city, captured_pokemon)"
                      f" VALUES ('{self.name}', '{self.city}', '{str1}' )")
-
-                print(query)
+                #print(query)
 
                 cursor.execute(query)
                 pokemon_db.commit()
-
                 print("\nTable updated, 1 row affected.")
 
         except Exception as ermsg:
@@ -84,11 +76,21 @@ class Player:
             player = cursor.execute(query)
 
             column = cursor.description
+            count = 0
 
-            for item in player:
-                print(column[0][0]+": " + item.player_name + column[1][0] + ": "
-                                        + item.city + column[2][0] + ": " + item.captured_pokemon)
-                #break
+            for row in player:
+                count = count + 1
+                Player(row.player_name,row.city,row.captured_pokemon)
+
+            player = cursor.execute(query)
+            length = 0
+
+            for last_row in player:
+                length = length + 1
+                if length == count:
+                    print(column[0][0]+": " + last_row.player_name + column[1][0] + ": "
+                                         + last_row.city + column[2][0] + ": " + last_row.captured_pokemon)
+                    break
 
             print("\nOperation has been completed.")
 
@@ -96,4 +98,3 @@ class Player:
             print("\nPânico !! ! !! The data has not been read. Please refer to error message.")
             print(ermsg)
             raise
-
--- pokemon_class.py
+++ pokemon_class.py
@@ -1,10 +1,10 @@
 from player_class import Player
+from python_to_db_connection import *
 
-class Pokemon(Player):
+class Pokemon:
     def __init__(self):
-        super().__init__()
-
         self.pokemon_name = ''
+
 
     def pokemon_tackle(self):
         pass
@@ -16,8 +16,26 @@ class Pokemon(Player):
         pass
 
     def save_pokemon(self):
-        pass
+        # a_pokemon = Player('','')
+        # #a_pokemon.search_for_pokemon()
+
+
+        try:
+            query = (f"INSERT INTO Pokemon (pokemon_name)"
+                     f" VALUES ('{self}' )")
+
+            cursor.execute(query)
+            pokemon_db.commit()
+
+            print("\nPokemon Table updated, 1 row affected.")
+
+        except Exception as ermsg:
+            print("\nPânico !! ! !!")
+            print(ermsg)
+            raise
+
+
+
 
     def load_a_pokemon(self):
         pass
-
